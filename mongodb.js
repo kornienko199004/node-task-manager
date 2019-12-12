85f1d4c8e956f4c5a952cd3de9606a0827612c93
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-12 10:19:32 +0300

    Start to add collections

--- mongodb.js
+++ mongodb.js
@@ -6,15 +6,67 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database');
-    }
+MongoClient.connect(
+    connectionURL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+        if (error) {
+            return console.log('Unable to connect to database');
+        }
 
-    console.log('Connected correctly!');
-    const db = client.db(databaseName);
-    db.collection('users').insertOne({
-        name: 'Aleksey',
-        age: 29
-    });
-});
+        console.log('Connected correctly!');
+        const db = client.db(databaseName);
+        db.collection('tasks').insertMany(
+            [
+                {
+                    description: 'First task',
+                    completed: false,
+                },
+                {
+                    description: 'Second task',
+                    completed: false,
+                },
+                {
+                    description: 'Third task',
+                    completed: false,
+                },
+            ],
+            (error, result) => {
+                if (error) {
+                    return console.log('Unable to insert task');
+                }
+
+                console.log(result.ops);
+            }
+        );
+        // db.collection('users').insertMany([
+        //     {
+        //         name: 'Jen',
+        //         age: 28,
+        //     },
+        //     {
+        //         name: 'Gunter',
+        //         age: 27,
+        //     }],
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert user');
+        //         }
+
+        //         console.log(result.ops);
+        //     });
+        // db.collection("users").insertOne(
+        //   {
+        //     name: "Aleksey",
+        //     age: 29
+        //   },
+        //   (error, result) => {
+        //       if (error) {
+        //           return console.log('Unable to insert user');
+        //       }
+
+        //       console.log(result.ops);
+        //   }
+        // );
+    }
+);
