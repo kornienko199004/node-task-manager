27b8e12ec7655c14428d7ef6d01b6c23a3f02c40
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-12 16:00:49 +0300

    The objectId chapter

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,20 @@
 // CRUD
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id);
+console.log(id.id.length);
+console.log(id);
+console.log(id.getTimestamp());
+console.log(id.toHexString());
 
 MongoClient.connect(
     connectionURL,
@@ -16,29 +26,29 @@ MongoClient.connect(
 
         console.log('Connected correctly!');
         const db = client.db(databaseName);
-        db.collection('tasks').insertMany(
-            [
-                {
-                    description: 'First task',
-                    completed: false,
-                },
-                {
-                    description: 'Second task',
-                    completed: false,
-                },
-                {
-                    description: 'Third task',
-                    completed: false,
-                },
-            ],
-            (error, result) => {
-                if (error) {
-                    return console.log('Unable to insert task');
-                }
+        // db.collection('tasks').insertMany(
+        //     [
+        //         {
+        //             description: 'First task',
+        //             completed: false,
+        //         },
+        //         {
+        //             description: 'Second task',
+        //             completed: false,
+        //         },
+        //         {
+        //             description: 'Third task',
+        //             completed: false,
+        //         },
+        //     ],
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert task');
+        //         }
 
-                console.log(result.ops);
-            }
-        );
+        //         console.log(result.ops);
+        //     }
+        // );
         // db.collection('users').insertMany([
         //     {
         //         name: 'Jen',
@@ -55,18 +65,19 @@ MongoClient.connect(
 
         //         console.log(result.ops);
         //     });
-        // db.collection("users").insertOne(
-        //   {
-        //     name: "Aleksey",
-        //     age: 29
-        //   },
-        //   (error, result) => {
-        //       if (error) {
-        //           return console.log('Unable to insert user');
-        //       }
+        // db.collection('users').insertOne(
+        //     {
+        //         _id: id,
+        //         name: 'Andrew',
+        //         age: 34,
+        //     },
+        //     (error, result) => {
+        //         if (error) {
+        //             return console.log('Unable to insert user');
+        //         }
 
-        //       console.log(result.ops);
-        //   }
+        //         console.log(result.ops);
+        //     }
         // );
     }
 );
