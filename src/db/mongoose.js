b2d2c43424ae62778714aaceef8a2184cc9f1650
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-16 09:03:13 +0300

    Add validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,16 +6,40 @@ mongoose.connect('mongodb://127.0.0.1:27017/tasks-manager-api', {
     useCreateIndex: true,
 });
 
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String,
+//     },
+//     completed: {
+//         type: Boolean,
+//     }
+// });
+
+// const task = new Task({ description: 'Make homework', completed: false });
+// task.save()
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
 const User = mongoose.model('User', {
     name: {
         type: String,
+        required: true,
     },
     age: {
         type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
     },
 });
 
-const me = new User({ name: 'Mike', age: 29 });
+const me = new User({ name: 'Mike', age: -1 });
 me.save()
     .then(res => {
         console.log(res);
