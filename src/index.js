2dcb3c253cf7c033e74cfbf8434b884d7ae93582
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-20 11:18:47 +0300

    Separate routes from index.js

--- src/index.js
+++ src/index.js
@@ -1,83 +1,16 @@
 const express = require('express');
-
 require('./db/mongoose');
-const Task = require('./models/task');
+
 const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
+app.use(taskRouter);
 
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
-    if (!tasks) {
-      res.status(404).send();
-    }
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-    res.send(task);
-    if (!task) {
-      res.status(404).send();
-    }
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.post('/tasks', async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.patch('/tasks/:id', async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ['description', 'completed'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' });
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-    res.send(task);
-    if (!task) {
-      res.status(404).send();
-    }
-  } catch (e) {
-    res.status(500).send();
-  }
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      res.status(404).send();
-    }
-    res.send(task);
-  } catch (e) {
-    res.status(500).send();
-  }
-});
 
 app.listen(port, () => {
   console.log('Server is up on port', port);
