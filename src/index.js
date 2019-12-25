ba7280cd4da221ec394c6fbc48a731edbf3e6807
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2019-12-25 09:16:02 +0300

    Hide some user data

--- src/index.js
+++ src/index.js
@@ -15,15 +15,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('Server is up on port', port);
 });
-
-// const jwt = require('jsonwebtoken');
-
-// const myFunction = async () => {
-//   const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '0 seconds' });
-//   console.log(token);
-
-//   const data = jwt.verify(token, 'thisismynewcourse');
-//   console.log(data);
-// };
-
-// myFunction();
